b550287a4d2a280102b369ba0649daf264367b57
Anysg <2801772839@qq.com>
2022-08-23 17:21:03 +0800

    更新测试

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import * as express from 'express';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true })); // For parsing application/x-www-form-urlencoded
+
+  const options = new DocumentBuilder()
+    .setTitle('欢迎来到我的博客后台 API管理页面')
+    .setDescription('Welcome to the Blog API')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
